b5058bc5a52f3d347de5802eff966329f695cc1c
Vincent Dumont <dumontv@outlook.com>
2017-01-10 19:58:47 -0500

    Added player movement and input detection

---
+++ Assets/Scripts/Actors/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _playerSpeed = 3.5f;
+
+    private Rigidbody2D _rigidbody2D;
+    private InputManager _inputManager;
+
+	private void Start()
+	{
+	    _rigidbody2D = GetComponent<Rigidbody2D>();
+	    _inputManager = GetComponent<InputManager>();
+	    _inputManager.OnMouseLeftClick += OnMouseLeftClick;
+	}
+
+    private void OnMouseLeftClick(Vector2 pos)
+    {
+        //Debug.Log(String.Format("Player pos: {0}, Mouse pos: {1}", transform.position, pos));
+        StopAllCoroutines();
+        StartCoroutine(MoveToClickCoroutine(pos));
+    }
+
+    private IEnumerator MoveToClickCoroutine(Vector2 pos)
+    {       
+        while ((Vector2) transform.position != pos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pos, _playerSpeed * Time.deltaTime);
+            yield return null;
+        }           
+    }
+}
---
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+    public delegate void OnMouseLeftClickHandler(Vector2 pos);
+    public event OnMouseLeftClickHandler OnMouseLeftClick;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            OnMouseLeftClick(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }          
+    }
+}
